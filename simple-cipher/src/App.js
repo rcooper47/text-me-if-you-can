bf6da42b7b4de9cd6783b0873ef09cc67bfeaac9
ryan cooper <3RCOOPER8@GMAIL.COM>
2022-08-10 18:21:38 -0400

    Initial React Setup, Input Field

---
+++ simple-cipher/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Cipher from './Cipher.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Cipher id='Cipher'></Cipher>
+    </div>
+  );
+}
+
+export default App;
